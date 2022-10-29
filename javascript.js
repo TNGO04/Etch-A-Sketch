68ae348c0b37b26e2bbe4e8bc86e6482c53f053b
Trang <trangkch1997@gmail.com>
2022-10-28 22:37:55 -0400

    Add and implement a grid size input so user can input the grid-size they want.
    Grid size input describes how many blocks are along the width and height of the board. Using an event listener, when the grid size input is changed, the board is reset and new grid blocks according to the grid size is added to the board node.

--- javascript.js
+++ javascript.js
@@ -1,64 +1,41 @@
 const boardObject = document.querySelector(".board");
-const dimension = 14;
+const sizeInput = document.querySelector("#grid-size");
+
+// change grid size whenever grid-size input is changed
+let dimension = sizeInput.value;
+sizeInput.addEventListener('change', changeGridSize);
+
+// default color button is black
 let lastClicked = document.getElementById('black');
 initializeBoard(dimension);
 
 // set color mode based on color buttons
 const colorButtons = document.querySelectorAll(".color-pick");
-colorButtons.forEach((button) => button.addEventListener('click', (event) => {
-    buttonUnclicked(lastClicked);
-    lastClicked = button;
-    buttonClicked(lastClicked);
-    
-}));
+setUpColorButtons();
 
-const blockList = document.querySelectorAll(".grid-block");
+// set up event listeners to implement sketching 
+let blockList = document.querySelectorAll(".grid-block");
 let mouseDown = false;
-//deal with etching through mousedown, mouseover (hover), and mouseup event
-blockList.forEach((block) => {
-        block.addEventListener('mousedown', (event) => {
-            mouseDown = true;
-            fillColor(event.target);
+setUpGridEventListener();
 
-        });
-
-        block.addEventListener('mouseover', (event) => {
-            if (mouseDown) {
-                fillColor(event.target);
-            }
-        });
-
-        block.addEventListener('mouseup', (event) => {
-            mouseDown = false;
-        });
-        /*
-        block.addEventListener('mouseenter', (event) => {
-            event.target.style.borderWidth = '0.5px';
-            event.target.style.borderColor = getRandomColor();
-        });
-
-        block.addEventListener('mouseleave', (event) => {
-            event.target.style.borderColor = 'transparent';
-        });*/
-
-    }
-);
-const container = document.querySelector('body');
-container.addEventListener('mouseup', () => {
-    mouseDown = false;
-})
-
-
+// implement clear button functionality
 const clearButton = document.getElementById('clear');
 clearButton.addEventListener('click', clearBoard);
 
 
-
+/**
+ * Create grid and initialize default color button.
+ * @param {number} size   width and height of sketch board
+ */
 function initializeBoard(size) {
     createGrid(size);
     buttonClicked(lastClicked); // set style for lastClicked
 }
 
+/**
+ * Create grid of certain size by appending grid block into board.
+ * @param {number} size 
+ */
 function createGrid(size) {
     let gridBlock, rowElement;
 
@@ -78,11 +55,87 @@ function createGrid(size) {
     }
 }
 
-
-function fillColor(block) {
-    block.style.backgroundColor = getColor();
+/**
+ * Set up event listener for color buttons (rainbow, black, color picker, and erase).
+ */
+function setUpColorButtons() {
+    colorButtons.forEach((button) => button.addEventListener('click', (event) => {
+        buttonUnclicked(lastClicked);
+        lastClicked = button;
+        buttonClicked(lastClicked);
+        
+    }));
 }
 
+/**
+ * Set up event listeners for grid blocks.
+ */
+function setUpGridEventListener() {
+
+    //deal with etching through mousedown, mouseover (hover), and mouseup event
+    blockList.forEach((block) => {
+        block.addEventListener('mousedown', (event) => {
+            mouseDown = true;
+            event.target.style.backgroundColor = getColor();
+
+        });
+
+        block.addEventListener('mouseover', (event) => {
+            if (mouseDown) {
+                event.target.style.backgroundColor = getColor();
+            }
+        });
+
+        block.addEventListener('mouseup', (event) => {
+            mouseDown = false;
+        });
+        /*
+        block.addEventListener('mouseenter', (event) => {
+            event.target.style.borderWidth = '0.5px';
+            event.target.style.borderColor = getRandomColor();
+        });
+
+        block.addEventListener('mouseleave', (event) => {
+            event.target.style.borderColor = 'transparent';
+        });*/
+
+    }
+    );
+    const container = document.querySelector('body');
+    container.addEventListener('mouseup', () => {
+    mouseDown = false;
+    })
+
+}
+
+/**
+ * Remove all children nodes of an element.
+ * Source: https://www.javascripttutorial.net/dom/manipulating/remove-all-child-nodes/
+ * @param {node} parent 
+ */
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+/**
+ * Change grid size by clearing current grid blocks and adding new ones.
+ */
+function changeGridSize() {
+    removeAllChildNodes(boardObject);
+    initializeBoard(sizeInput.value);
+    lastClicked = document.getElementById('black');
+
+    blockList = document.querySelectorAll(".grid-block");
+    setUpGridEventListener();
+}
+
+/**
+ * Get color based on current color mode. 
+ * @returns color for grid block
+ */
 function getColor() {
     switch (lastClicked.id) {
         case 'black':
@@ -102,21 +155,36 @@ function getColor() {
     return color;
 }
 
+/**
+ * Change style of button after being clicked.
+ * @param {Node} button 
+ */
+
 function buttonClicked(button) {
     button.style.borderStyle = 'inset';
     button.style.fontWeight = 'bold';
 }
+
+/**
+ * Change style of button to be unclicked.
+ * @param {Node} button 
+ */
+
 
 function buttonUnclicked(button) {
     button.style.borderStyle = 'solid';
     button.style.fontWeight = 'normal';
 }
 
-
+/**
+ * Get a random pastel/light color.
+ * @returns random color
+ */
 function getRandomColor() {
-    // for this random color generator, I only want pastel-ish colors (for aesthetic reason). By only generating random value 
-    // from shift to 256, I can avoid the lower RGB values which are associated with darker/bolder colors.
-    shift = 160;
+    /* for this random color generator generate colors from 3 R, B, and G values. I only want pastel-ish colors 
+    (for aesthetic reason), so by only generating random value from shift to 256, I can avoid the lower RGB values 
+    which are associated with darker/bolder colors.*/
+    shift = 100;
     var color = 'rgb(';
     for (var i = 0; i < 3; i++) {
       color += Math.floor(shift + Math.random() * (256 - shift));
@@ -128,6 +196,9 @@ function getRandomColor() {
     return color;
   }
   
+  /**
+   * Clear color from board.
+   */
   function clearBoard() {
     blockList.forEach((block) => block.style.backgroundColor = 'white');
   }
